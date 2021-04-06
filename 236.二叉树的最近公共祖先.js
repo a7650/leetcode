417dbc6530cc4d7df80bd6f67c09385bbf31e0e0
zzp <43945674+a7650@users.noreply.github.com>
2021-04-06 17:00:24 +0800

    update

---
+++ 236.二叉树的最近公共祖先.js
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=236 lang=javascript
+ *
+ * [236] 二叉树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  if (!root) return null
+  let pPath = []
+  let qPath = []
+  const getPath = (node, path) => {
+    if (node) {
+      path = [...path, node.val]
+      if (node.val === p) {
+        pPath = path
+      } else if (node.val === q) {
+        qPath = path
+      }
+      if (!pPath.length || !qPath.length) {
+        getPath(node.right, [...path])
+        getPath(node.left, [...path])
+      }
+    }
+  }
+  getPath(root, [])
+  console.log(pPath, qPath)
+  const map = pPath.reduce((pre, cur) => {
+    return {
+      ...pre,
+      [cur]: 1
+    }
+  }, {})
+  for (let i = 0; i < qPath.length; i++) {
+    if (map[qPath[i]]) {
+      return qPath[i]
+    }
+  }
+}
+
+function TreeNode(val, left, right) {
+  this.val = val
+  this.left = left
+  this.right = right
+}
+const tree = new TreeNode(3, 5, 1)
+console.log(lowestCommonAncestor(tree, 5, 1))
+// @lc code=end
