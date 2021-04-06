db69d3148af997494d1d26cfcb9a9c750ba0fd60
zzp <43945674+a7650@users.noreply.github.com>
2021-04-06 09:28:09 +0800

    update

--- 二叉树/257.二叉树的所有路径.js
+++ 二叉树/257.二叉树的所有路径.js
@@ -16,23 +16,57 @@
 /**
  * @param {TreeNode} root
  * @return {string[]}
+ * 深度优先搜索
+ */
+// var binaryTreePaths = function (root) {
+//   const rst = []
+//   const track = (node, path) => {
+//     if (node) {
+//       path += String(node.val)
+//       if (node.left === null && node.right === null) {
+//         rst.push(path)
+//       } else {
+//         path += '->'
+//         track(node.left, path)
+//         track(node.right, path)
+//       }
+//     }
+//   }
+//   track(root, '')
+//   return rst
+// }
+
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ * 广度优先搜索
  */
 var binaryTreePaths = function (root) {
-  const rst = []
-  const track = (node, path) => {
-    if (node === null) {
-      rst.push(path.join('->'))
-      return
-    }
-    if (node.val !== null) {
-      node.left !== null && track(node.left, [...path, node.val])
-      node.right !== null && track(node.right, [...path, node.val])
-      if (node.left === null && node.right === null) {
-        rst.push([...path, node.val].join('->'))
+  const paths = []
+  if (root === null) {
+    return paths
+  }
+  const node_queue = [root]
+  const path_queue = [root.val.toString()]
+
+  while (node_queue.length) {
+    const node = node_queue.shift()
+    const path = path_queue.shift()
+
+    if (node.left === null && node.right === null) {
+      paths.push(path)
+    } else {
+      if (node.left !== null) {
+        node_queue.push(node.left)
+        path_queue.push(path + '->' + node.left.val.toString())
+      }
+
+      if (node.right !== null) {
+        node_queue.push(node.right)
+        path_queue.push(path + '->' + node.right.val.toString())
       }
     }
   }
-  track(root, [])
-  return rst
+  return paths
 }
 // @lc code=end
