22a71378f7f50b46b1ce6d1f25c9742e561e1efe
ZZP <43945674+a7650@users.noreply.github.com>
2021-05-13 19:34:05 +0800

    update

---
+++ 链表/141.环形链表.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=javascript
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  if (!head) return false
+  if (head.flag) return true
+  if (!head.next) return false
+  head.flag = true
+  return hasCycle(head.next)
+}
+// @lc code=end
