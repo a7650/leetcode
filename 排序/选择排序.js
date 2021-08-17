66fb93fe369abba1fb5d5236283b981e422f17ac
zzp <43945674+a7650@users.noreply.github.com>
2021-08-18 00:20:19 +0800

    update

---
+++ 排序/选择排序.js
@@ -0,0 +1,17 @@
+function sort(arr) {
+  const len = arr.length
+  for (let i = 0; i < len; i++) {
+    let minIdx = i
+    for (let j = i; j < len; j++) {
+      if (arr[j] < arr[minIdx]) {
+        minIdx = j
+      }
+    }
+    if (minIdx !== i) {
+      ;[arr[i], arr[minIdx]] = [arr[minIdx], arr[i]]
+    }
+  }
+  return arr
+}
+
+console.log(sort([5,4,3,2,1,6]))
